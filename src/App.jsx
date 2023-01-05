f4e781502b26ed0204e70d2240d091a18331ecf6
kajalyadavgithub <kajalyadav20020411@gmail.com>
2023-01-05 12:05:27 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import AllRoutes from "./Components/AllRoutes";
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<AllRoutes />
+			<Footer/>
 		</>
 	);
 }
