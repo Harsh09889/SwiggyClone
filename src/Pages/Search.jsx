678c1e2e4fc6b74fd6644928e48416ae44346248
JagguHakeem1610 <hakeem938121@gmail.com>
2023-01-07 18:32:16 +0530

    Changes Made by Jagadish

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -78,14 +78,10 @@ const Search = () => {
 						placeholder='Search for restaurants and food...'
 					/>
 					{
-						<SearchIcon
-							style={{
-								fontSize: "32px",
-								fontWeight: "lighter",
-								color: "rgba(0,0,0,0.50)",
-								margin: "0 1rem",
-							}}
-						/>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="mr-4 w-6 h-6 ">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+					  </svg>
+					  
 					}
 				</div>
 				{/* <div className='my-8 flex gap-4'>
