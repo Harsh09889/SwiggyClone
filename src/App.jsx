67d8c5f4667d5323bb7591e70a6be87c2eac3682
Harsh <harsh09889@gmail.com>
2023-01-07 15:25:04 +0530

    mui removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import AllRoutes from "./Components/AllRoutes";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import "react-loading-skeleton/dist/skeleton.css";
-
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import LoginSignup from "./Components/Login-Signup/LoginSignup";
 
 function App() {
+	const [switchLogin, setSwitchLogin] = useState(false);
+	const isAuth = useSelector((state) => state.auth.auth.isAuth);
 	return (
 		<>
+			<LoginSignup
+				switchLogin={switchLogin}
+				setSwitchLogin={setSwitchLogin}
+			/>
+
 			<Navbar />
 			<AllRoutes />
-			<Footer/>
+			<Footer />
 		</>
 	);
 }
