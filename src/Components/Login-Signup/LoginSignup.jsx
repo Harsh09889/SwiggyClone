0b02b7f6da8e891103779b40c672645825a888e8
Harsh <harsh09889@gmail.com>
2023-01-08 16:01:11 +0530

    login working

--- src/Components/Login-Signup/LoginSignup.jsx
+++ src/Components/Login-Signup/LoginSignup.jsx
@@ -1,228 +1,279 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import {
-  Box,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerHeader,
-  DrawerBody,
-  Button,
-  Text,
-  DrawerCloseButton,
-  Image,
-  Input,
+	Box,
+	Drawer,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerHeader,
+	DrawerBody,
+	Button,
+	Text,
+	DrawerCloseButton,
+	Image,
+	Input,
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import { useState } from "react";
 import CustomFont from "./customFont";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../Redux/Auth/AuthActions";
+import { useNavigate } from "react-router-dom";
 function LoginSignup({ open, loadlogin }) {
-  const [switchLogin, setSwitchLogin] = useState(loadlogin);
+	const [switchLogin, setSwitchLogin] = useState(loadlogin);
+	const [loginState, setLoginState] = useState({
+		phn: 0,
+		password: "",
+	});
 
-  useEffect(() => {
-    if (isOpen) onClose();
-    else onOpen();
-  }, [open]);
+	const [signupState, setSignupState] = useState({
+		phn: 0,
+		username: "",
+		name: "",
+		password: "",
+	});
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
+	const navigate = useNavigate();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const auth = useSelector((state) => state.auth.auth.isAuth);
+	const error = useSelector((state) => state.auth.auth.error);
+	const firstRender = useRef(0);
+	const dispatch = useDispatch();
 
-  const handlespanClick = () => {
-    setSwitchLogin(true);
-  };
-  //signup switch
-  const handlespanSignUp = () => {
-    setSwitchLogin(false);
-  };
+	function handleLogin() {
+		if (!loginState.phn || !loginState.password) {
+			alert("Please Enter Both Phone and Password");
+		} else {
+			dispatch(login({ ...loginState, phn: +loginState.phn }));
+		}
+	}
 
-  return (
-    <Box>
-      {/* <Box display={"flex"} gap={"20px"} justifyContent={"center"}>
-        <Button mb={"100px"} borderRadius={"0px"} onClick={onOpen}>
-          Login
-        </Button>
-        <Button
-          borderRadius={"0px"}
-          color={"white"}
-          colorScheme={"black"}
-          bg={"black"}
-          onClick={onOpen}
-        >
-          Sign up
-        </Button>
-      </Box> */}
-      <Drawer onClose={onClose} isOpen={isOpen} size={"md"}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
+	useEffect(() => {
+		if (firstRender.current > 1) {
+			console.log(firstRender.current);
+			if (auth) {
+				alert("Succesfully Logged in");
+				onClose();
+				setLoginState({
+					phn: 0,
+					password: "",
+				});
+				navigate("/");
+			} else {
+				error && alert(error);
+			}
+		}
 
-          <DrawerHeader></DrawerHeader>
+		firstRender.current++;
+	}, [auth, error]);
 
-          <DrawerBody>
-            {switchLogin ? (
-              <>
-                <Box
-                  display={"flex"}
-                  justifyContent={"space-between"}
-                  w={"80%"}
-                  mt={"5%"}
-                >
-                  <Box>
-                    <Text color={"black"} fontSize={"33px"}>
-                      Login
-                    </Text>
-                    <Text>
-                      or{" "}
-                      <span
-                        onClick={handlespanSignUp}
-                        style={{
-                          color: "#fc8019",
-                          cursor: "pointer",
-                          fontSize: "14px",
-                        }}
-                      >
-                        create an account
-                      </span>
-                    </Text>
-                  </Box>
-                  <Box w={"28%"}>
-                    <Image
-                      src={
-                        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r"
-                      }
-                    ></Image>
-                  </Box>
-                </Box>
-                <Box w={"72%"}>
-                  <Input
-                    placeholder={"Phone number"}
-                    padding={"34px"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    type={"number"}
-                    mt={"40px"}
-                  />
-                  <Input
-                    placeholder={"Password"}
-                    padding={"34px"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    type={"password"}
-                    mb={"24px"}
-                  />
-                  <Button
-                    fontSize={"14px"}
-                    colorScheme={"#fc8019"}
-                    fontWeight={"bold"}
-                    color={"white"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    bg={"#fc8019"}
-                    padding={"27px"}
-                  >
-                    LOGIN
-                  </Button>
-                  <Text mt={"5px"} fontSize={"12px"}>
-                    By clicking on Login, I accept the Terms & Conditions &
-                    Privacy Policy
-                  </Text>
-                </Box>
-              </>
-            ) : (
-              //SIGNUP
-              <>
-                <Box
-                  display={"flex"}
-                  justifyContent={"space-between"}
-                  w={"80%"}
-                  mt={"5%"}
-                >
-                  <Box>
-                    <Text color={"black"} fontSize={"33px"}>
-                      Sign up
-                    </Text>
-                    <Text>
-                      <span
-                        onClick={handlespanClick}
-                        style={{
-                          color: "#fc8019",
-                          cursor: "pointer",
-                          fontSize: "14px",
-                        }}
-                      >
-                        login to your account
-                      </span>
-                    </Text>
-                  </Box>
-                  <Box w={"28%"}>
-                    <Image
-                      src={
-                        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r"
-                      }
-                    ></Image>
-                  </Box>
-                </Box>
-                <Box w={"72%"}>
-                  <Input
-                    placeholder={"Phone number"}
-                    padding={"34px"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    type={"number"}
-                    mt={"40px"}
-                  />
+	useEffect(() => {
+		if (isOpen) onClose();
+		else onOpen();
+	}, [open]);
 
-                  <Input
-                    placeholder={"Email"}
-                    padding={"34px"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    type={"email"}
-                  />
-                  <Input
-                    placeholder={"User_Name"}
-                    padding={"34px"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    type={"text"}
-                  />
-                  <Input
-                    placeholder={"Password"}
-                    padding={"34px"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    type={"password"}
-                  />
-                  <Text
-                    color={"#5D8ED5"}
-                    mt={"25px"}
-                    mb={"16px"}
-                    fontSize={"16px"}
-                  >
-                    Have a referral code?
-                  </Text>
+	//signup switch
+	const handlespanSignUp = () => {
+		setSwitchLogin(false);
+	};
 
-                  <Button
-                    colorScheme={"#fc8019"}
-                    fontSize={"14px"}
-                    fontWeight={"bold"}
-                    color={"white"}
-                    borderRadius={"0px"}
-                    w={"100%"}
-                    bg={"#fc8019"}
-                    padding={"27px"}
-                  >
-                    CONTINUE
-                  </Button>
-                  <Text mt={"5px"} fontSize={"12px"}>
-                    By creating an account, I accept the Terms & Conditions &
-                    Privacy Policy
-                  </Text>
-                </Box>
-              </>
-            )}
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </Box>
-  );
+	const handlespanClick = () => {
+		setSwitchLogin(true);
+	};
+	return (
+		<Box>
+			<Drawer
+				onClose={onClose}
+				isOpen={isOpen}
+				size={"md"}>
+				<DrawerOverlay />
+				<DrawerContent>
+					<DrawerCloseButton />
+
+					<DrawerHeader></DrawerHeader>
+
+					<DrawerBody>
+						{switchLogin ? (
+							<>
+								<Box
+									display={"flex"}
+									justifyContent={"space-between"}
+									w={"80%"}
+									mt={"5%"}>
+									<Box>
+										<Text
+											color={"black"}
+											fontSize={"33px"}>
+											Login
+										</Text>
+										<Text>
+											or{" "}
+											<span
+												onClick={handlespanSignUp}
+												style={{
+													color: "#fc8019",
+													cursor: "pointer",
+													fontSize: "14px",
+												}}>
+												create an account
+											</span>
+										</Text>
+									</Box>
+									<Box w={"28%"}>
+										<Image
+											src={
+												"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r"
+											}></Image>
+									</Box>
+								</Box>
+								<Box w={"72%"}>
+									<Input
+										placeholder={"Phone number"}
+										padding={"34px"}
+										borderRadius={"0px"}
+										w={"100%"}
+										type={"number"}
+										value={loginState.phn || ""}
+										name='phn'
+										onChange={(e) =>
+											setLoginState((p) => ({
+												...p,
+												[e.target.name]: e.target.value,
+											}))
+										}
+										mt={"40px"}
+									/>
+									<Input
+										placeholder={"Password"}
+										padding={"34px"}
+										name='password'
+										onChange={(e) =>
+											setLoginState((p) => ({
+												...p,
+												[e.target.name]: e.target.value,
+											}))
+										}
+										value={loginState.password}
+										borderRadius={"0px"}
+										w={"100%"}
+										type={"password"}
+										mb={"24px"}
+									/>
+									<Button
+										fontSize={"14px"}
+										colorScheme={"#fc8019"}
+										fontWeight={"bold"}
+										color={"white"}
+										borderRadius={"0px"}
+										onClick={handleLogin}
+										w={"100%"}
+										bg={"#fc8019"}
+										padding={"27px"}>
+										LOGIN
+									</Button>
+									<Text
+										mt={"5px"}
+										fontSize={"12px"}>
+										By clicking on Login, I accept the Terms & Conditions &
+										Privacy Policy
+									</Text>
+								</Box>
+							</>
+						) : (
+							//SIGNUP
+							<>
+								<Box
+									display={"flex"}
+									justifyContent={"space-between"}
+									w={"80%"}
+									mt={"5%"}>
+									<Box>
+										<Text
+											color={"black"}
+											fontSize={"33px"}>
+											Sign up
+										</Text>
+										<Text>
+											<span
+												onClick={handlespanClick}
+												style={{
+													color: "#fc8019",
+													cursor: "pointer",
+													fontSize: "14px",
+												}}>
+												login to your account
+											</span>
+										</Text>
+									</Box>
+									<Box w={"28%"}>
+										<Image
+											src={
+												"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r"
+											}></Image>
+									</Box>
+								</Box>
+								<Box w={"72%"}>
+									<Input
+										placeholder={"Phone number"}
+										padding={"34px"}
+										borderRadius={"0px"}
+										w={"100%"}
+										type={"number"}
+										mt={"40px"}
+									/>
+
+									<Input
+										placeholder={"Email"}
+										padding={"34px"}
+										borderRadius={"0px"}
+										w={"100%"}
+										type={"email"}
+									/>
+									<Input
+										placeholder={"User_Name"}
+										padding={"34px"}
+										borderRadius={"0px"}
+										w={"100%"}
+										type={"text"}
+									/>
+									<Input
+										placeholder={"Password"}
+										padding={"34px"}
+										borderRadius={"0px"}
+										w={"100%"}
+										type={"password"}
+									/>
+									<Text
+										color={"#5D8ED5"}
+										mt={"25px"}
+										mb={"16px"}
+										fontSize={"16px"}>
+										Have a referral code?
+									</Text>
+
+									<Button
+										colorScheme={"#fc8019"}
+										fontSize={"14px"}
+										fontWeight={"bold"}
+										color={"white"}
+										borderRadius={"0px"}
+										w={"100%"}
+										bg={"#fc8019"}
+										padding={"27px"}>
+										CONTINUE
+									</Button>
+									<Text
+										mt={"5px"}
+										fontSize={"12px"}>
+										By creating an account, I accept the Terms & Conditions &
+										Privacy Policy
+									</Text>
+								</Box>
+							</>
+						)}
+					</DrawerBody>
+				</DrawerContent>
+			</Drawer>
+		</Box>
+	);
 }
 export default LoginSignup;
